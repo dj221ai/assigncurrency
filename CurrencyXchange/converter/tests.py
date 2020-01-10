090c3e45d6085fa9a3b330b976faf4b33973dccd
pratham <prathamopenschool1@pratham.org>
2020-01-10 17:42:16 +0530

    test for user

--- CurrencyXchange/converter/tests.py
+++ CurrencyXchange/converter/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import json
+import requests
+from django.contrib.auth.models import User
+from .serializers import UserCreationSerializer, CurrencyConversionSerializer
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class UserCreationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {'first_name': "tester", 'last_name': 'admin', 'username': 'testeradmin',
+                'password': 'strong_password'}
+        response = self.client.post('/users/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
